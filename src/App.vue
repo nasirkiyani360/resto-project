4a32a63bdb51e8b54443baa4a931c6e0eac609d8
nasirniaz <nasir.niaz@sixlogics.com>
2022-11-03 20:18:33 +0500

    initial code pushed.

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img class="logo" src="./assets/resto-logo.png"/>
+  <SignUp/>
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue';
+
+export default {
+  name: 'App',
+  components :{
+    SignUp
+  }
+}
+</script>
+
+<style>
+.logo
+{
+  width: 150px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>Sign Up</h2>
+    <div class="register">
+        <input type="text" placeholder="Enter Name" />
+        <input type="text" placeholder="Enter Email" />
+        <input type="password" placeholder="Enter Password" />
+        <button type="submit">Sign Up</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SignUp'
+}
+</script>
+<style scoped>
+.register input {
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+
+.register button {
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: #fff;
+    cursor: pointer;
+}
+</style>
